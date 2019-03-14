752858afb19ebc05dda281a5ec5afea9bac1e89b
harysonii <haryson.onuoha@gmail.com>
2019-03-14 21:54:32 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -2,27 +2,33 @@
 const express = require('express'); //project uses the express server
 const bodyParser = require('body-parser');
 const graphqlHTTP = require('express-graphql');
+const graphql = require('graphql')
+
+const {send} = require ('micro');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const helmet = require('helmet');
+require('now-env');
 
 
 const app = express();
 
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', 'localhost:3000');
-//     res.setHeader('Access-Control-Allow_Methods', 'POST, GET, OPTIONS');
-//     res.setHeader('Access-Control_Allow-Headers', 'Content-Type, Authorization');
-//     if(req.method === 'OPTIONS') {
-//         return res.sendStatus(200);
-//     }
-//     next();
-// })
-app.use(cors());
+
+
+/*app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'localhost:3000');
+    res.setHeader('Access-Control-Allow_Methods', 'POST, GET, OPTIONS');
+    res.setHeader('Access-Control_Allow-Headers', 'Content-Type, Authorization');
+    if(req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+})*/
+app.use(cors({credentials: true, origin: true, allowedHeaders: ['Content-Type', 'Authorization']}));
+app.options('*', cors());
 app.use(helmet());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 
 
 /*app.use(session({
@@ -42,20 +48,21 @@ app.use((req, res, next) => {
     next();
 });*/
 
-app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: true
+	   
+
+ app.use('/', graphqlHTTP({
+    schema: schema,
+	graphiql: true
 })
 );
 
-      
-    mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@gothere-kluc7.gcp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`,
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@gothere-kluc7.gcp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`,
  { useNewUrlParser: true })
     .then( () => { 
-		app.listen(process.env.PORT || 4000);
+		app.listen(4000);
 		console.log('Ready');
 		})
     .catch(err => {console.log(err);
        });
-  
     
